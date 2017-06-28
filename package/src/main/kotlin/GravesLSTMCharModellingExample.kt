271305c830a8b19ff92795a49bd45ebba7b1ba33
gimpei <gim.kobayashi@gmail.com>
2017-06-28 21:39:30 +0900

    toriisogi

--- package/src/main/kotlin/GravesLSTMCharModellingExample.kt
+++ package/src/main/kotlin/GravesLSTMCharModellingExample.kt
@@ -7,12 +7,14 @@ import org.deeplearning4j.nn.conf.MultiLayerConfiguration
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
 import org.deeplearning4j.nn.conf.Updater
 import org.deeplearning4j.nn.conf.layers.GravesLSTM
+import org.deeplearning4j.nn.conf.layers.DenseLayer
 import org.deeplearning4j.nn.conf.layers.RnnOutputLayer
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.api.IterationListener
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.deeplearning4j.parallelism.ParallelWrapper
+import org.deeplearning4j.nn.conf.layers.OutputLayer
 import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.api.buffer.DataBuffer
 import org.nd4j.linalg.api.buffer.util.DataTypeUtil
@@ -36,19 +38,20 @@ import kotlin.coroutines.experimental.*
 
 object Dammy {
   fun next() = buildSequence {
-    val mm   = mutableMapOf<Int, List<Pair<List<Int>,List<Int>>>>()
-    val txts = File("data/DATASET.txt").readText().split("\n").filter { it.length != 0 }
-    val size = txts.size
+    val mm    = mutableMapOf<Int, List<Pair<List<Int>,List<Int>>>>()
+    val txts  = File("data/DATASET.txt").readText().split("\n").filter { it.length != 0 }
+    val size  = txts.size
     var scope = 0
-    while(true) {   
+    (0..20000).map {   
       // setting dimention size
       // first  1. batch size
       // second 2. char(or word) dimentions
       // second 3. length of sentence
       // initialize here
-      val input : INDArray = Nd4j.zeros( 32, 100, 31)
-      val label : INDArray = Nd4j.zeros( 32, 100, 31)
-      (scope..scope+31).map { count ->
+      val WIDTH            = 500
+      val input : INDArray = Nd4j.zeros( WIDTH, 100, 31 )
+      val label : INDArray = Nd4j.zeros( WIDTH, 100, 31 )
+      (scope..scope+100).map { count ->
         val hl   = txts[count].split(",")
         val head = hl[0].split(" ").map { it.toInt() }
         val tail = hl[1].split(" ").map { it.toInt() }
@@ -60,118 +63,80 @@ object Dammy {
         }
         val last = hl[1].map { it.toInt() }
       }
-      scope += 32
-      if(scope + 32> size)
-        scope = 0
+      scope += WIDTH
+      if(scope + WIDTH > size) scope = 0
       val ds = DataSet( input, label )
       println( "shape ${input.shape().toList()}" )
       yield( ds )
     }
   }
 }
+
 object GravesLSTMCharModelling {
-	@JvmStatic
-  fun main( args : Array<String> ) {
-		val lstmLayerSize = 200
-		val miniBatchSize = 32
-		val exampleLength = 1000
+  fun train() {
+    val lstmLayerSize = 200
+    val miniBatchSize = 32
+    val exampleLength = 1000
     val tbpttLength   = 50
-		val numEpochs     = 1
+    val numEpochs     = 1
     val generateSamplesEveryNMinibatches = 10
-		val nSamplesToGenerate = 4
-		val nCharactersToSample = 300
-		var generationInitialization:String? = null
-		val rng = Random(12345)
-		val conf = NeuralNetConfiguration.Builder()
-			.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
-			.learningRate(0.1)
-			.rmsDecay(0.95)
-			.seed(12345)
-			.regularization(true)
-			.l2(0.001)
+    val nSamplesToGenerate = 4
+    val nCharactersToSample = 300
+    var generationInitialization:String? = null
+    val rng = Random(12345)
+    val conf = NeuralNetConfiguration.Builder()
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
+      .learningRate(0.1)
+      .rmsDecay(0.95)
+      .seed(12345)
+      .regularization(true)
+      .l2(0.001)
          .weightInit(WeightInit.XAVIER)
          .updater(Updater.RMSPROP)
-			.list()
-			.layer(0, GravesLSTM.Builder().nIn( 100 ).nOut(lstmLayerSize)
-				.activation(Activation.TANH).build())
-			.layer(1, GravesLSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
-				.activation(Activation.TANH).build())
-			.layer(2, RnnOutputLayer.Builder(LossFunction.MCXENT).activation(Activation.SOFTMAX)
-				.nIn(lstmLayerSize).nOut( 100 ).build())
-        .backpropType(BackpropType.TruncatedBPTT).tBPTTForwardLength(tbpttLength).tBPTTBackwardLength(tbpttLength)
-			.pretrain(false).backprop(true)
-			.build()
-
-		val net = MultiLayerNetwork(conf)
-		net.init()
-		net.setListeners( ScoreIterationListener(1) )
-
+      .list()
+      .layer(0, GravesLSTM.Builder()
+                  .nIn( 100 )
+                  .nOut( lstmLayerSize ) 
+                  .activation( Activation.TANH).build() )
+      .layer(1, GravesLSTM.Builder()
+                  .nIn( lstmLayerSize )
+                  .nOut( lstmLayerSize )
+                  .activation( Activation.TANH).
+                  build() )
+      .layer(2, RnnOutputLayer
+                  .Builder( LossFunction.MCXENT )
+                  .nIn( lstmLayerSize )
+                  .nOut( 100 )
+                  .activation( Activation.SOFTMAX )
+                  .build() )
+      .backpropType(BackpropType.TruncatedBPTT)
+          .tBPTTForwardLength(tbpttLength)
+          .tBPTTBackwardLength(tbpttLength)
+      .pretrain(false)
+      .backprop(true)
+      .build()
+    val net = MultiLayerNetwork(conf)
+    net.init()
+    net.setListeners( ScoreIterationListener(1) )
     for( ds in Dammy.next() ) { 
       net.fit( ds )
     }
-    /*
-		MultiLayerNetwork net = new MultiLayerNetwork(conf);
-		net.init();
-		net.setListeners(new ScoreIterationListener(1), new IterationListener() {
-            @Override
-            public boolean invoked() {
-                return true;
-            }
-
-            @Override
-            public void invoke() {
-
-            }
-
-            @Override
-            public void iterationDone(Model model, int iteration) {
-                System.out.println("--------------------");
-                System.out.println("Sampling characters from network given initialization \"" + (generationInitialization == null ? "" : generationInitialization) + "\"");
-                String[] samples = sampleCharactersFromNetwork(generationInitialization, (MultiLayerNetwork) model,iter,rng,nCharactersToSample,nSamplesToGenerate);
-                for( int j = 0; j<samples.length; j++) {
-                    System.out.println("----- Sample " + j + " -----");
-                    System.out.println(samples[j]);
-                    System.out.println();
-                }
-            }
-        });
-
-
-        //Print the  number of parameters in the network (and for each layer)
-		Layer[] layers = net.getLayers();
-		int totalNumParams = 0;
-		for( int  i= 0; i < layers.length; i++) {
-			int nParams = layers[i].numParams();
-			System.out.println("Number of parameters in layer " + i + ": " + nParams);
-			totalNumParams += nParams;
-		}
-
-		System.out.println("Total number of network parameters: " + totalNumParams);
-
-
-        // ParallelWrapper will take care of load balancing between GPUs.
-        ParallelWrapper wrapper = new ParallelWrapper.Builder(net)
-            // DataSets prefetching options. Set this value with respect to number of actual devices
-            .prefetchBuffer(24)
-
-            // set number of workers equal or higher then number of available devices. x1-x2 are good values to start with
-            .workers(4)
-
-            // rare averaging improves performance, but might reduce model accuracy
-            .averagingFrequency(3)
-
-            // if set to TRUE, on every averaging model score will be reported
-            .reportScoreAfterAveraging(true)
-
-            // optinal parameter, set to false ONLY if your system has support P2P memory access across PCIe (hint: AWS do not support P2P)
-            .useLegacyAveraging(true)
-
-            .build();
-
-        wrapper.fit(iter);
-
-
-		System.out.println("\n\nExample complete");
-    */
+    val layers = net.getLayers()
+    var totalNumParams = 0
+    for( i in (0..layers.size-1)) {
+      val nParams = layers[i].numParams()
+      println("Number of parameters in layer $i $nParams")
+      totalNumParams += nParams
+    }
+    println("Total number of network parameters: $totalNumParams")
+    println("Complete");
+  }
+	@JvmStatic
+  fun main( args : Array<String> ) {
+    val argments = args.toList()
+    when { 
+      argments.contains("train") -> train()
+      else                       -> train()
+    }
 	}
 }
